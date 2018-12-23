009c13b089d411b06c74377725e8d1a41819a44e
lgcolella <luigicolella.1997@gmail.com>
2018-12-23 22:00:16 +0100

    refactor: Added background texture and fixed text overflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,7 @@ module.exports = {
   siteMetadata: {
     title: 'Luigi Colella',
     description: 'Logbook of a software developer',
-    author: 'lcolella',
-    socials: {
-      github: '',
-      linkedin: ''
-    }
+    author: 'lcolella'
   },
   plugins,
 }
